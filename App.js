66a80dfbbdefb160a52a2940e3624395609032db
nishaasigamani <108520824+nishaasigamani@users.noreply.github.com>
2023-04-04 22:44:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+    <header className="App-header">
+     <h1> Let we see the output of JAVASCRIPT</h1>
+     <button onClick={window['check']}>Click here</button>
+    </header>
+  </div>
+
+  );
+}
+
+export default App;
